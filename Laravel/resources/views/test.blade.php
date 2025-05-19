399d65c8b87d0867813b3c0c090b265d9cadb4ef
Gp2Oak <situacaodesafio03@gmail.com>
2025-05-19 14:37:04 -0300

    Subindo no Laravel

---
+++ Laravel/resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Teste OK</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+    <h1 class="text-4xl font-bold">Laravel na pasta /Laravel funcionando! 🚀</h1>
+</body>
+</html>
